a8c28ccc783ed822c74e41fd67973346ca3b3bcc
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2026-01-26 19:58:19 -0500

    permissions stuff

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 exit # This is not a script, just snippets.
 
+echo $CONTAINER_NAME
 CONTAINER_NAME=midi
 
 HOST=/home/jeff/code/midi
@@ -9,16 +10,19 @@ docker run --name "$CONTAINER_NAME" -it -d               \
   --network host              \
   --platform linux/amd64      \
   --user 1000:1000            \
-  --group-add audio           \
-  --device /dev/snd           \
   --dns 8.8.8.8 --dns 1.1.1.1 \
   -v "$HOST":/home/ubuntu     \
+  --group-add $(getent group audio | cut -d: -f3) \
+  --device /dev/snd           \
   jeffreybbrown/hode:latest
-  # The DNS bits, somehow, permit Claude Code to
-  # work through my phone's mobile hotspot.
-  # --newtowrk host binds each port
-  # to the host's port of the same number,
-  # e.g. 1729=1729 (TypeDB).
+
+  # Above:
+  # The --group-add line dynamically gets the host audio group GID.
+  # The --dns line somehow lets Claude Code
+  #   work through my phone's mobile hotspot.
+  #   --newtowrk host binds each port
+  #   to the host's port of the same number,
+  #   e.g. 1729=1729 (TypeDB).
 
 docker start $CONTAINER_NAME
 docker exec -it $CONTAINER_NAME bash
